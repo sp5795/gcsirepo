0e6db1628a8cdab596a8e71771da11aa250f47bc
sp5795 <sp5795@g.rit.edu>
2025-09-09 10:04:01 +0400

    py2

---
+++ test3.py
@@ -0,0 +1,14 @@
+def hello():
+    name=input("enter your name:")
+    print("hello", name,"!")
+
+def box():
+    print("******")
+    print("*    *")
+    print("******")
+
+def birthdate():
+    print("January 2nd, 2008")
+
+def days_of_week():
+    print("Sunday, Monday")
