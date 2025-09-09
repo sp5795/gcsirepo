3a8f35bf01e0727ec25ce6b2844a143fe83e68bb
sp5795 <sp5795@g.rit.edu>
2025-09-09 09:56:03 +0400

    py1

---
+++ variables.py
@@ -0,0 +1,14 @@
+def variable_practice():
+    age=int(input("enter your age: "))
+    print("your age in months: ",age*12)
+
+    days=365
+    print("number of days in a year are: ",days)
+
+    first_pet=input("enter the name of your first pet: ")
+    print("name of your first pet is: ",first_pet)
+
+    pi=3.14159
+    print("first five digits of pi are: ",pi)
+
+variable_practice()
